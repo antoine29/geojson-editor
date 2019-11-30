8f6efa58771a8b3bbd9cbc153e890ca85d58b0aa
Antoine <anthonyfg9@gmail.com>
2019-11-30 19:30:25 -0400

    adding tabbed menu

--- src/components/Sidebar.vue
+++ src/components/Sidebar.vue
@@ -1,22 +1,25 @@
 <template>
   <i-col span="10" class="sidebar">
-    <top-menu />
-    <code-area />
+    <!-- <top-menu /> -->
+    <TabMenu />
+    <!-- <code-area /> -->
     <bottom-menu />
   </i-col>
 </template>
 
 <script>
-import CodeArea from './Code.vue'
-import TopMenu from './TopMenu.vue'
+// import CodeArea from './Code.vue'
+// import TopMenu from './TopMenu.vue'
 import BottomMenu from './BottomMenu.vue'
+import TabMenu from './TabMenu';
 
 export default {
   name: 'Sidebar',
   components: {
-    CodeArea,
+    TabMenu,
+    // CodeArea,
     BottomMenu,
-    TopMenu
+    // TopMenu
   }
 }
 </script>
---
+++ src/components/TabMenu.vue
@@ -0,0 +1,59 @@
+<template>
+  <div class="tabMenu">
+    <button
+      v-for="tab in tabs"
+      :key="tab"
+      :class="['tab-btn', { active: selected === tab }]"
+      @click="selected = tab;"
+    >
+      {{ tab }}
+    </button>
+
+    <component :is="selected" class="tab" />
+  </div>
+</template>
+
+<script>
+import Form from "./Form";
+import CodeArea from './Code'
+
+export default {
+	components: {
+    Form,
+    CodeArea
+	},
+	data: function() {
+		return {
+			tabs: ["Form","CodeArea"],
+			selected: "Form"
+		};
+	}
+};
+</script>
+
+<style>
+  .tabMenu{
+    /* padding: 10px; */
+    /* background: #FFDC80; */
+    width: 100%;
+  }
+
+  .tab-btn {
+    padding: 6px 10px;
+    background: #ffffff;
+    cursor: pointer;
+    /* margin-bottom: 1rem; */
+    /* border: 2px solid #cccccc; */
+    outline: none;
+  }
+
+  .active {
+    border-bottom: 3px solid green;
+    background: #ffff25;
+  }
+
+  .tab {
+    /* border: 1px solid #ccc; */
+    padding: 10px;
+  }
+</style>
